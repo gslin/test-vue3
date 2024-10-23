9d21e2e9807a3faa9f4547b83aa75fbd6ceda76c
Gea-Suan Lin <gslin@gslin.org>
2024-10-23 22:39:30 +0800

    feat: implement a simple form w/ uploading function.

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,19 @@
 <template>
+<div class="container mx-auto">
+  <h1 class="font-bold text-3xl">File Upload</h1>
+  <div class="my-2">
+    <form action="https://httpbin.org/anything" id="image-upload-form" method="post">
+      <div class="flex flex-col gap-2">
+        <div>
+          <input class="bg-gray-100 border rounded-lg p-2" multiple name="files" type="file" />
+        </div>
+        <div>
+          <input class="bg-gray-100 border rounded-lg p-2" name="submit" type="submit" />
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
 </template>
 
 <script setup lang="ts">
