d96b0864223195b885cea4b6ef0d6a996ecb6def
Gea-Suan Lin <gslin@gslin.org>
2024-10-25 03:48:20 +0800

    feat: implement custom uploading.

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,40 @@
 <div class="container mt-4 mx-auto">
   <h1 class="font-bold text-3xl">File Upload</h1>
   <div class="my-2">
-    <form action="https://httpbin.org/anything" id="image-upload-form" method="post">
-      <div class="flex flex-col gap-2">
+    <form>
+      <div class="flex flex-col gap-4">
+        <h2>Area</h2>
         <div>
-          <input class="bg-gray-100 border rounded-lg p-2" multiple name="files" type="file" />
+          <file-pond
+            allow-multiple="true"
+            class="w-full"
+            credits="false"
+            label-idle="Drag files &amp; drag to here, or click this area"
+            name="tFiles"
+            ref="pond"
+            server="https://httpbin.org/anything"
+            :allow-reorder="true"
+            :files="tFiles"
+            :instant-upload="false"
+          >
+          </file-pond>
         </div>
         <div>
-          <input class="bg-gray-100 border rounded-lg p-2" name="submit" type="submit" />
+          <h2>Information</h2>
+
+          <div class="flex gap-2">
+            <textarea class="bg-gray-100 border font-mono h-[20rem] rounded-lg p-2 w-full" readonly>{{ pond }}</textarea>
+            <textarea class="bg-gray-100 border font-mono h-[20rem] rounded-lg p-2 w-full" readonly>{{ JSON.stringify(tFiles?.files) }}</textarea>
+          </div>
+        </div>
+        <div>
+          <h2>Buttons</h2>
+          <input
+            class="bg-gray-100 border rounded-lg p-2"
+            name="submit"
+            type="submit"
+            @click.prevent="uploadFiles"
+          />
         </div>
       </div>
     </form>
@@ -18,10 +45,35 @@
 
 <script setup lang="ts">
 /* vue */
-import { onMounted } from 'vue';
+import { ref } from 'vue';
 
-onMounted(() => {
-});
+/* 3rd-party */
+import axios from 'axios';
+import vueFilePond from 'vue-filepond';
+import 'filepond/dist/filepond.min.css';
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
+
+/* refs */
+const tFiles = ref([]);
+const pond = ref();
+
+/* handlers */
+const uploadFiles = () => {
+  const fd = new FormData();
+  for (const file of pond.value.getFiles()) {
+    console.log(file);
+    fd.append('files[]', file.file);
+  }
+
+  axios.post('https://httpbin.org/anything', fd, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+};
+
+const FilePond = vueFilePond(FilePondPluginImagePreview);
 </script>
 
 <style scoped>
